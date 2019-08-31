aefc421dcb93d18e25c5e2c235aae9de089e8164
Oogapooki <andrewosborn101@gmail.com>
2019-08-31 01:22:19 -0500

    employee and manager server info

--- schema.sql
+++ schema.sql
@@ -4,24 +4,145 @@ CREATE DATABASE everoot_db;
 
 USE everoot_db;
 
-CREATE TABLE products(
+/*Manager view*/
+
+/*Energy Conservation Tracker*/
+
+CREATE TABLE ec_tracker(
 item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-product_name VARCHAR(50) NOT NULL,
-department_name VARCHAR(50) NOT NULL,
-price DECIMAL(10,2) DEFAULT 0,
-stock_quantity INT DEFAULT 0
+month_name VARCHAR(50) NOT NULL,
+energy_expenditure VARCHAR(50) NOT NULL,
+expense DECIMAL(10,2) DEFAULT 0
+/* stock_quantity INT DEFAULT 0 */
 );
 
-INSERT INTO products(product_name,department_name,price,stock_quantity)
-VALUES ("Tic Tacs", "Food", 1.50, 100),
-	("T-shirt","Clothing",12.40,50),
-       ("PS4","Entertainment",300.00,30),
-       ("Recipe Book","Cooking",9.95,75),
-       ("Heaphones","Audio", 19.99,20),
-       ("Flat-Screen TV","Entertainment",400.00,10),
-       ("Boxing Gloves","Fitness",15.00,8),
-       ("TV Dinner","Food",6.00,50),
-       ("Bananas","Food",2.00,500),
-       ("Electric Razor","Toiletries",50.00,18);
+INSERT INTO ec_tracker(month_name,energy_expenditure,expense)
+VALUES ("January", "10.399 kWh", 110.21),
+	   ("February","15.5 kWh",0),
+       ("March","",0),
+       ("April","",0),
+       ("May","", 0),
+       ("June","",0),
+       ("July","",0),
+       ("August","",0),
+       ("September","",0),
+       ("October","",0),
+       ("November","",0),
+       ("December","",0);
 
-SELECT * FROM products;
+
+SELECT FORMAT(SUM(energy_expenditure),
+2)total FROM ec_tracker; 
+
+/*SELECT * FROM ec_tracker;*/
+
+/*Water Conservation Tracker */
+
+CREATE TABLE wc_tracker(
+item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+month_name VARCHAR(50) NOT NULL,
+water_expenditure VARCHAR(50) NOT NULL,
+expense DECIMAL(10,2) DEFAULT 0
+);
+
+INSERT INTO wc_tracker(month_name,water_expenditure,expense)
+VALUES ("January", "95 gallons", 80),
+	   ("February","",0),
+       ("March","",0),
+       ("April","",0),
+       ("May","", 0),
+       ("June","",0),
+       ("July","",0),
+       ("August","",0),
+       ("September","",0),
+       ("October","",0),
+       ("November","",0),
+       ("December","",0);
+ /*      ("Total water used in the last fiscal year","",0); */
+
+/*SELECT * FROM wc_tracker;*/
+
+/*Waste Reduction Tracker*/
+
+CREATE TABLE wr_tracker(
+item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+month_name VARCHAR(50) NOT NULL,
+trash_bags DECIMAL(10) DEFAULT 0,
+compost_bins DECIMAL(10) DEFAULT 0,
+recycle_bins DECIMAL(10) DEFAULT 0,
+hazardous_waste_bins DECIMAL(10) DEFAULT 0,
+totals DECIMAL(10) DEFAULT 0
+
+);
+
+INSERT INTO wr_tracker(month_name,trash_bags,compost_bins,recycle_bins,hazardous_waste_bins,totals)
+VALUES ("January", 6, 4,6,1,17),
+	   ("February",0,0,0,0,0),
+       ("March",0,0,0,0,0),
+       ("April",0,0,0,0,0),
+       ("May",0,0,0,0,0),
+       ("June",0,0,0,0,0),
+       ("July",0,0,0,0,0),
+       ("August",0,0,0,0,0),
+       ("September",0,0,0,0,0),
+       ("October",0,0,0,0,0),
+       ("November",0,0,0,0,0),
+       ("December",0,0,0,0,0);
+ /*      ("Total waste",0,0,0,0,0); */
+
+/*SELECT * FROM wr_tracker;*/
+
+/* Carbon Reduction Tracker */
+
+CREATE TABLE cr_tracker(
+item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+month_name VARCHAR(50) NOT NULL,
+carbon_expenditure VARCHAR(50) NOT NULL,
+offset_cost DECIMAL(10,2) DEFAULT 0
+);
+
+INSERT INTO cr_tracker(month_name,carbon_expenditure,offset_cost)
+VALUES ("January", "16.4 metric tons", 28),
+	   ("February","",0),
+       ("March","",0),
+       ("April","",0),
+       ("May","", 0),
+       ("June","",0),
+       ("July","",0),
+       ("August","",0),
+       ("September","",0),
+       ("October","",0),
+       ("November","",0),
+       ("December","",0);
+/*       ("Total carbon expenditure","",0); */
+
+/*SELECT * FROM cr_tracker;*/
+
+/* Employee Data */
+
+CREATE TABLE employee_data(
+item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+month_name VARCHAR(50) NOT NULL,
+commute_distance DECIMAL(10) NOT NULL,
+travel_to_work_days DECIMAL(10) DEFAULT 0
+);
+
+INSERT INTO employee_data(month_name,commute_distance,travel_to_work_days)
+VALUES ("January", 6, 20),
+	   ("February",0,0),
+       ("March",0,0),
+       ("April",0,0),
+       ("May",0, 0),
+       ("June",0,0),
+       ("July",0,0),
+       ("August",0,0),
+       ("September",0,0),
+       ("October",0,0),
+       ("November",0,0),
+       ("December",0,0);
+/*       ("Total carbon expenditure","",0); */
+
+/* SELECT AVG(commute_distance)'Average commute distance' FROM employee_data; */
+
+/* SELECT * FROM employee_data; */
+
